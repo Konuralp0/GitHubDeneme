a1d60dc7fbba2956d0bdb931695af7b58699113b
konuralp <konuralpcelikyay@gmail.com>
2022-04-11 19:44:00 +0200

    tekrar

--- src/test/java/Deneme/GithubDeneme.java
+++ src/test/java/Deneme/GithubDeneme.java
@@ -3,6 +3,5 @@ package Deneme;
 public class GithubDeneme {
     public static void main(String[] args) {
         System.out.println("Ilk gonderim");
-        System.out.println("ikinci ");
     }
 }
